3bce7fc7d2c81b9e4b14f639b5bc96250e4ab524
deshpabh <abhijeetdeshpande1996@gmail.com>
2021-10-18 21:17:28 +0530

    Interface documentation

--- src/main/java/com/example/ParallelFramework/parallelclasses/CustomExecutor.java
+++ src/main/java/com/example/ParallelFramework/parallelclasses/CustomExecutor.java
@@ -1,7 +1,12 @@
 package com.example.ParallelFramework.parallelclasses;
 
 import com.example.ParallelFramework.context.AbstractContext;
-
+/*
+@Author : Abhijeet Deshpande
+THis interface has 4 defaults methods mocking crud operations.
+Write your code with implementing these methods and just use call() method to run this in single instance
+For parallel execution pass the concrete instance of this class to the Parallel Coordinator instance.
+ */
 public interface CustomExecutor {
     void initializeData(AbstractContext abstractContext);
     void commitData(AbstractContext abstractContext);
